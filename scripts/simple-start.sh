c5ec852e8fab41c350b20382b096a5beb775ad73
Reed Rosenbluth <331327+reedrosenbluth@users.noreply.github.com>
2021-02-22 11:06:07 -0500

    add NAT'ed startup scripts

--- scripts/simple-start.sh
+++ scripts/simple-start.sh
@@ -44,6 +44,36 @@ case "$1" in
         wait_node || exit_error "node failed to boot up"
         tail -f $STACKS_FOLLOWER_LOGFILE
         ;;
+    miner_nat)
+        miner_config "miner" "20443" "20444"
+        STACKS_MINER_PUBLIC_IP="$2"
+        BITCOIN_PUBLIC_IP="$3"
+        STACKS_MINER_BOOTSTRAP_IP="$3"
+        STACKS_MINER_BOOTSTRAP_PORT=20444
+        FAUCET_URL="http://$3:$FAUCET_PORT"
+        CHAINSTATE_DIR="/tmp/chainstate"
+        PROCESS_EXIT_AT_BLOCK_HEIGHT=5000
+        set_nat "true"
+        start_node &
+        PID=$!
+        wait_node || exit_error "node failed to boot up"
+        tail -f $STACKS_MINER_LOGFILE
+        ;;
+    follower_nat)
+        follower_config "follower" "20443" "20444"
+        STACKS_FOLLOWER_PUBLIC_IP="$2"
+        STACKS_FOLLOWER_BOOTSTRAP_IP="$3"
+        STACKS_FOLLOWER_BOOTSTRAP_PORT=20444
+        BITCOIN_PUBLIC_IP="$3"
+        FAUCET_URL="http://$3:$FAUCET_PORT"
+        CHAINSTATE_DIR="/tmp/chainstate"
+        PROCESS_EXIT_AT_BLOCK_HEIGHT=5000
+        set_nat "true"
+        start_node &
+        PID=$!
+        wait_node || exit_error "node failed to boot up"
+        tail -f $STACKS_FOLLOWER_LOGFILE
+        ;;
     *)
         exit 1
         ;;
